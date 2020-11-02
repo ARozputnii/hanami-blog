16d1522739a1396816aa1196dbb5a8816619206a
ARozputnii <arozputnii@gmail.comt@gitlab.com:everlabs/everlabs-website.gitset :rvm_ruby_version, 2.6.5set :user, deployer>
2020-11-03 00:04:20 +0200

    index book

--- apps/web/controllers/books/index.rb
+++ apps/web/controllers/books/index.rb
@@ -1,10 +1,13 @@
 module Web
   module Controllers
-    module Home
+    module Books
       class Index
         include Web::Action
 
+        expose :books
+
         def call(params)
+          @books = BookRepository.new.all
         end
       end
     end
